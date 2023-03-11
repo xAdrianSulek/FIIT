d39b616f16f22156cc3fda584b3979554aea02f2
xAdrianSulek <Adriann.sulek@gmail.com>
2023-03-11 16:11:37 +0100

    Avl tree

--- DSA/BTS_AVL/BinarySearchTree_AVL/BinarySearchTree_AVL/Program.cs
+++ DSA/BTS_AVL/BinarySearchTree_AVL/BinarySearchTree_AVL/Program.cs
@@ -1,273 +1,224 @@
 ﻿using System;
 
-namespace BinarySearchTree_AVL
+
+namespace TryingShit
 {
-class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            AVL tree = new AVL();
-            tree.Add(5);
-            tree.Add(3);
-            tree.Add(7);
-            tree.Add(10);
-            tree.Add(15);
-            tree.Add(32);
-            tree.Add(1);
-            tree.Add(8);
-            tree.Add(11);
-            tree.DisplayTree();
+            AVLTree avlTree = new AVLTree();
+            avlTree.add(25);
+            avlTree.add(70);
+            avlTree.add(20);
+            avlTree.add(10);
+            avlTree.add(15);
+            avlTree.display();
         }
     }
-    class AVL
+
+
+    /// <summary>
+    /// KYSNEM SA FR
+    /// </summary>
+    class AVLTree
     {
-        class Node
+
+        internal class Node
         {
+            public int height;
             public int data;
             public Node left;
             public Node right;
+
             public Node(int data)
             {
                 this.data = data;
+                
+            }
+            public Node()
+            {
             }
         }
-        Node root;
-        public AVL()
+        Node parent;
+        
+        public void add(int data)
         {
+            
+            if (parent == null)
+            {
+                parent = new Node(data);
+            }
+            else
+            {
+                insert(parent,data);
+            }
         }
         
-        public void Add(int data)
+        
+        public void insert(Node currentNode, int data)
         {
-            Node newItem = new Node(data);
-            if (root == null)
+            if (data < currentNode.data)
             {
-                root = newItem; 
-            }
-            else
-            {
-                root = RecursiveInsert(root, newItem);
-            }
-        }
-        private Node RecursiveInsert(Node current, Node n)
-        {
-            if (current == null)
-            {
-                current = n;
-                return current;
-            }
-            else if (n.data < current.data)
-            {
-                current.left = RecursiveInsert(current.left, n);
-                current = balance_tree(current);
-            }
-            else if (n.data > current.data)
-            {
-                current.right = RecursiveInsert(current.right, n);
-                current = balance_tree(current);
-            }
-            return current;
-        }
-        private Node balance_tree(Node current)
-        {
-            int b_factor = balance_factor(current);
-            if (b_factor > 1)
-            {
-                if (balance_factor(current.left) > 0)
+                if (currentNode.left == null)
                 {
-                    current = RotateLL(current);
+                    currentNode.left = new Node(data);   
                 }
                 else
                 {
-                    current = RotateLR(current);
+                    insert(currentNode.left, data);
+                    currentNode = balance(currentNode);
                 }
             }
-            else if (b_factor < -1)
+            
+            else if (data > currentNode.data)
             {
-                if (balance_factor(current.right) > 0)
+                if (currentNode.right == null)
                 {
-                    current = RotateRL(current);
+                    currentNode.right = new Node(data);
                 }
                 else
                 {
-                    current = RotateRR(current);
+                    insert(currentNode.right, data);
+                    currentNode = balance(currentNode);
                 }
             }
-            return current;
+            
         }
-        public void Delete(int target)
-        {//and here
-            root = Delete(root, target);
-        }
-        private Node Delete(Node current, int target)
+        
+        
+        public Node balance(Node node)
         {
-            Node parent;
-            if (current == null)
-            { return null; }
-            else
+            int HeightDiff = heightDifference(node);
+            
+            if (HeightDiff > 1)
             {
-                //left subtree
-                if (target < current.data)
+                if (heightDifference(node.left) > 0)
                 {
-                    current.left = Delete(current.left, target);
-                    if (balance_factor(current) == -2)//here
-                    {
-                        if (balance_factor(current.right) <= 0)
-                        {
-                            current = RotateRR(current);
-                        }
-                        else
-                        {
-                            current = RotateRL(current);
-                        }
-                    }
-                }
-                //right subtree
-                else if (target > current.data)
-                {
-                    current.right = Delete(current.right, target);
-                    if (balance_factor(current) == 2)
-                    {
-                        if (balance_factor(current.left) >= 0)
-                        {
-                            current = RotateLL(current);
-                        }
-                        else
-                        {
-                            current = RotateLR(current);
-                        }
-                    }
-                }
-                //if target is found
-                else
-                {
-                    if (current.right != null)
-                    {
-                        //delete its inorder successor
-                        parent = current.right;
-                        while (parent.left != null)
-                        {
-                            parent = parent.left;
-                        }
-                        current.data = parent.data;
-                        current.right = Delete(current.right, parent.data);
-                        if (balance_factor(current) == 2)//rebalancing
-                        {
-                            if (balance_factor(current.left) >= 0)
-                            {
-                                current = RotateLL(current);
-                            }
-                            else { current = RotateLR(current); }
-                        }
-                    }
-                    else
-                    {   //if current.left != null
-                        return current.left;
-                    }
-                }
-            }
-            return current;
-        }
-        public void Find(int key)
-        {
-            if (Find(key, root).data == key)
-            {
-                Console.WriteLine("{0} was found!", key);
-            }
-            else
-            {
-                Console.WriteLine("Nothing found!");
-            }
-        }
-        private Node Find(int target, Node current)
-        {
- 
-                if (target < current.data)
-                {
-                    if (target == current.data)
-                    {
-                        return current;
-                    }
-                    else
-                    return Find(target, current.left);
+                    node = rotationLeftLeft(node);
                 }
                 else
                 {
-                    if (target == current.data)
-                    {
-                        return current;
-                    }
-                    else
-                    return Find(target, current.right);
+                    node = rotationLeftRight(node);
                 }
-             
-        }
-        public void DisplayTree()
-        {
-            if (root == null)
-            {
-                Console.WriteLine("Tree is empty");
-                return;
             }
-            InOrderDisplayTree(root);
-            Console.WriteLine();
-        }
-        private void InOrderDisplayTree(Node current)
-        {
-            if (current != null)
+            
+            else if (HeightDiff < -1)
             {
-                InOrderDisplayTree(current.left);
-                Console.Write("({0}) ", current.data);
-                InOrderDisplayTree(current.right);
+                if (heightDifference(node.right) > 0)
+                {
+                    node = rotationRightLeft(node);
+                }
+                else
+                {
+                    node = rotationRightRight(node);
+                }
             }
+
+            return node;
         }
-        private int max(int l, int r)
+        
+
+        public int Height()
         {
-            return l > r ? l : r;
+            return HeightHelper(parent);
         }
-        private int getHeight(Node current)
+
+
+        public int HeightHelper(Node node)
         {
             int height = 0;
-            if (current != null)
+            if (node != null)
             {
-                int l = getHeight(current.left);
-                int r = getHeight(current.right);
-                int m = max(l, r);
-                height = m + 1;
+                int left = HeightHelper(node.left);
+                int right = HeightHelper(node.right);
+                height = Math.Max(left, right) + 1;
             }
             return height;
         }
-        private int balance_factor(Node current)
+
+        
+        /// 
+        /// Mozno bude lepsie storovat height v kazdom note. tato rekurzia moze trvat dlho
+        /// 
+        public int heightDifference(Node node)
         {
-            int l = getHeight(current.left);
-            int r = getHeight(current.right);
-            int b_factor = l - r;
-            return b_factor;
+            int left = HeightHelper(node.left);
+            int right = HeightHelper(node.right);
+            int heightDiff = left - right;
+            return heightDiff;
         }
-        private Node RotateRR(Node parent)
+        
+        
+        
+        public void displayHeight()
         {
-            Node pivot = parent.right;
-            parent.right = pivot.left;
-            pivot.left = parent;
-            return pivot;
+            Console.WriteLine(Height());
         }
-        private Node RotateLL(Node parent)
+        
+        
+        
+        public void display()
         {
-            Node pivot = parent.left;
-            parent.left = pivot.right;
-            pivot.right = parent;
-            return pivot;
+            if (parent == null)
+            {
+                Console.WriteLine("Strom je prazdny");
+            }
+            else
+            {
+                displayFromLevels(parent);
+            }
         }
-        private Node RotateLR(Node parent)
+
+        public void displayFromLevels(Node node)
         {
-            Node pivot = parent.left;
-            parent.left = RotateRR(pivot);
-            return RotateLL(parent);
+            
+            Console.Write(node.data + " ");
+            Console.Write(Height()+ "HEIGHT ");
+
+            if (node.left != null)
+            {
+                Console.Write(node.left.data + "L");
+                displayFromLevels(node.left);
+            }
+            if (node.right != null)
+            {
+                Console.Write(node.right.data + "R");
+                displayFromLevels(node.right);
+            }
         }
-        private Node RotateRL(Node parent)
+
+
+
+        public Node rotationRightRight(Node parentOfTriangle)
         {
-            Node pivot = parent.right;
-            parent.right = RotateLL(pivot);
-            return RotateRR(parent);
+            Node rotation = parentOfTriangle.right;
+            parentOfTriangle.right = rotation.left;
+            rotation.left = parentOfTriangle;
+            return rotation;
         }
+        
+        public Node rotationRightLeft(Node parentOfTriangle)
+        {
+            Node rotation = parentOfTriangle.right;
+            parentOfTriangle.right = rotationLeftLeft(rotation);
+            return rotationRightRight(parentOfTriangle);
+        }
+        
+        public Node rotationLeftLeft(Node parentOfTriangle)
+        {
+            Node rotation = parentOfTriangle.left;
+            parentOfTriangle.left = rotation.right;
+            rotation.right = parentOfTriangle;
+            return rotation;
+        }
+        
+        public Node rotationLeftRight(Node parentOfTriangle)
+        {
+            Node rotation = parentOfTriangle.left;
+            parentOfTriangle.left = rotationRightRight(rotation);
+            return rotationLeftLeft(parentOfTriangle);
+        }
+        
     }
 }
