607b864d140ae4dad06d1e6580ac5370a4dd1e8c
Adrián Šulek <91079104+xAdrianSulek@users.noreply.github.com>
2023-07-21 12:15:32 +0200

    Delete PROSIM directory

--- PROSIM/src/main/java/com/example/prosim/entity/Skeleton.java
+++
@@ -1,8 +0,0 @@
-package com.example.prosim.entity;
-
-public class Skeleton extends Enemy{
-    public Skeleton(int hp, int dmg, int speed) {
-        super(hp, dmg, speed);
-        this.name = "Skeleton";
-    }
-}
